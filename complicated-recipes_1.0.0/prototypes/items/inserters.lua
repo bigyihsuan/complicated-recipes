c9fcaebdfe8dd5de9b84b8384afbf0611e341ceb
bigyihsuan <bigyihsuan@gmail.com>
2016-04-23 08:56:49 -0400

    fix some things

--- complicated-recipes_1.0.0/prototypes/items/inserters.lua
+++ complicated-recipes_1.0.0/prototypes/items/inserters.lua
@@ -1,29 +1,34 @@
 data:extend({
 {
+	type = 'item',
 	name = 'burner-inserter-arm',
 	icon = "__complicated-recipes__/graphics/inserters/inserter-hand-0',
 	subgroup = 'intermediate-product',
 	stack_size = 50
 },
 {
+	type = 'item',
 	name = 'basic-inserter-arm',
 	icon = "__complicated-recipes__/graphics/inserters/inserter-hand-1',
 	subgroup = 'intermediate-product',
 	stack_size = 50
 },
 {
+	type = 'item',
 	name = 'long-armed-inserter-arm',
 	icon = "__complicated-recipes__/graphics/inserters/inserter-hand-2',
 	subgroup = 'intermediate-product',
 	stack_size = 50
 },
 {
+	type = 'item',
 	name = 'fast-inserter-arm',
 	icon = "__complicated-recipes__/graphics/inserters/inserter-hand-3',
 	subgroup = 'intermediate-product',
 	stack_size = 50
 },
 {
+	type = 'item',
 	name = 'smart-inserter-arm',
 	icon = "__complicated-recipes__/graphics/inserters/inserter-hand-4',
 	subgroup = 'intermediate-product',
