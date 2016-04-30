e6cb506266f3ceaccba91e7e0e11436c6b9217de
bigyihsuan <bigyihsuan@gmail.com>
2016-04-30 11:59:03 -0400

    added a bunch of table.insert() functions to add items to the technologies
    Please say that there's a better way.

---
+++ prototypes/technology.lua
@@ -0,0 +1,30 @@
+table.insert(data.raw['technology']['logistics'].effects, {type = 'unlock-recipe', name = 'basic-transport-belt-to-ground-hood'})
+table.insert(data.raw['tehcnology']['logistics'].effects, {type = 'unlock-recipe', name = 'basic-splitter-mechanism'})
+
+table.insert(data.raw['technology']['logistics-2'].effects, {type = 'unlock-recipe', name = 'fast-transport-belt-to-ground-hood'})
+table.insert(data.raw['tehcnology']['logistics-2'].effects, {type = 'unlock-recipe', name = 'fast-splitter-mechanism'})
+table.insert(data.raw['tehcnology']['logistics-2'].effects, {type = 'unlock-recipe', name = 'fast-belt-surface'})
+
+table.insert(data.raw['technology']['logistics-3'].effects, {type = 'unlock-recipe', name = 'express-transport-belt-to-ground-hood'})
+table.insert(data.raw['tehcnology']['logistics-3'].effects, {type = 'unlock-recipe', name = 'express-splitter-mechanism'})
+table.insert(data.raw['tehcnology']['logistics-3'].effects, {type = 'unlock-recipe', name = 'express-belt-surface'})
+
+table.insert(data.raw['technology']['automation'].effects, {type = 'unlock-recipe', name = 'assembling-machine-1-frame'})
+table.insert(data.raw['technology']['automation-2'].effects, {type = 'unlock-recipe', name = 'assembling-machine-2-frame'})
+table.insert(data.raw['technology']['automation-3'].effects, {type = 'unlock-recipe', name = 'assembling-machine-3-frame'})
+
+table.insert(data.raw['technology']['advanced-electronics'].effects, {type = 'unlock-recipe', name = 'advanced-circuit-board'})
+table.insert(data.raw['technology']['advanced-electronics-2'].effects, {type = 'unlock-recipe', name = 'processing-circuit-board'})
+
+table.insert(data.raw['tehcnology']['automation'].effects, {type = 'unlock-recipe', name = 'long-armed-inserter-arm'})
+table.insert(data.raw['tehcnology']['automation'].effects, {type = 'unlock-recipe', name = 'long-armed-inserter-platform'})
+table.insert(data.raw['tehcnology']['logistics'].effects, {type = 'unlock-recipe', name = 'fast-inserter-arm'})
+table.insert(data.raw['tehcnology']['logistics'].effects, {type = 'unlock-recipe', name = 'fast-inserter-platform'})
+table.insert(data.raw['tehcnology']['electronics'].effects, {type = 'unlock-recipe', name = 'smart-inserter-arm'})
+table.insert(data.raw['tehcnology']['electronics'].effects, {type = 'unlock-recipe', name = 'smart-inserter-platform'})
+
+table.insert(data.raw['technology']['military'].effects, {type = 'unlock-recipe', name = 'blank-shotgun-shell'})
+table.insert(data.raw['technology']['military-2'].effects, {type = 'unlock-recipe', name = 'piercing-bullet'})
+table.insert(data.raw['technology']['military-3'].effects, {type = 'unlock-recipe', name = 'blank-capsule'})
+
+table.insert(data.raw['technology']['oil-processing'].effects, {type = 'unlock-recipe', name = 'compressed-solid-fuel'})
