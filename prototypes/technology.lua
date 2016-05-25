927e29b7f5ad0d6e1048a4d07216be1bfb5f7ec9
bigyihsuan <bigyihsuan@gmail.com>
2016-05-24 23:20:59 -0400

    added module stuff to new format

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -2,15 +2,15 @@ recipes = {
 	["logistics"] = 'basic-transport-belt-to-ground-hood',
 	["logistics"] = 'basic-splitter-mechanism',
 	["logistics-2"] = 'fast-transport-belt-to-ground-hood',
-	["logistics-2"] = 'fast-splitter-mechanism', 
-	["logistics-2"] = 'fast-belt-surface', 
-	["logistics-3"] = 'express-transport-belt-to-ground-hood', 
-	["logistics-3"] = 'express-splitter-mechanism',   
-	["logistics-3"] = 'express-belt-surface',   
-	["automation"] = 'assembling-machine-1-frame',   
-	["automation-2"] = 'assembling-machine-2-frame',   
-	["automation-3"] = 'assembling-machine-3-frame',   
-	["advanced-electronics"] = 'advanced-circuit-board',   
+	["logistics-2"] = 'fast-splitter-mechanism',
+	["logistics-2"] = 'fast-belt-surface',
+	["logistics-3"] = 'express-transport-belt-to-ground-hood',
+	["logistics-3"] = 'express-splitter-mechanism',
+	["logistics-3"] = 'express-belt-surface',
+	["automation"] = 'assembling-machine-1-frame',
+	["automation-2"] = 'assembling-machine-2-frame',
+	["automation-3"] = 'assembling-machine-3-frame',
+	["advanced-electronics"] = 'advanced-circuit-board',
 	["advanced-electronics-2"] = 'processing-circuit-board',
 	["automation"] = 'long-armed-inserter-arm',
 	["automation"] = 'long-armed-inserter-platform',
@@ -19,43 +19,15 @@ recipes = {
 	["electronics"] = 'smart-inserter-arm',
 	["electronics"] = 'smart-inserter-platform',
 	["military"] = 'blank-shotgun-shell',
-	["military-2"] = 'piercing-bullet',  
+	["military-2"] = 'piercing-bullet',
 	["military-3"] = 'blank-capsule',
-	["oil-processing"] = 'compressed-solid-fuel' 	
+	["oil-processing"] = 'compressed-solid-fuel' ,
+	['speed-module'] = 'speed-augment',
+	['speed-module'] = 'speed-module-casing',
+	['productivity-module'] = 'productivity-augment',
+	['productivity-module'] = 'productivity-module-casing',
+	['effectivity-module'] = 'effectivity-augment',
+	['effectivity-module'] = 'effectivity-module-casing',
 }
-
+--['tech-name'] = 'recipe-name'
 addTechnologyRecipes(recipes)
-
-<<<<<<< HEAD
-table.insert(data.raw['technology']['logistics-3'].effects, {type = 'unlock-recipe', name = 'express-transport-belt-to-ground-hood'})
-table.insert(data.raw['tehcnology']['logistics-3'].effects, {type = 'unlock-recipe', name = 'express-splitter-mechanism'})
-table.insert(data.raw['tehcnology']['logistics-3'].effects, {type = 'unlock-recipe', name = 'express-belt-surface'})
-
-table.insert(data.raw['technology']['automation'].effects, {type = 'unlock-recipe', name = 'assembling-machine-1-frame'})
-table.insert(data.raw['technology']['automation-2'].effects, {type = 'unlock-recipe', name = 'assembling-machine-2-frame'})
-table.insert(data.raw['technology']['automation-3'].effects, {type = 'unlock-recipe', name = 'assembling-machine-3-frame'})
-
-table.insert(data.raw['technology']['advanced-electronics'].effects, {type = 'unlock-recipe', name = 'advanced-circuit-board'})
-table.insert(data.raw['technology']['advanced-electronics-2'].effects, {type = 'unlock-recipe', name = 'processing-circuit-board'})
-
-table.insert(data.raw['tehcnology']['automation'].effects, {type = 'unlock-recipe', name = 'long-armed-inserter-arm'})
-table.insert(data.raw['tehcnology']['automation'].effects, {type = 'unlock-recipe', name = 'long-armed-inserter-platform'})
-table.insert(data.raw['tehcnology']['logistics'].effects, {type = 'unlock-recipe', name = 'fast-inserter-arm'})
-table.insert(data.raw['tehcnology']['logistics'].effects, {type = 'unlock-recipe', name = 'fast-inserter-platform'})
-table.insert(data.raw['tehcnology']['electronics'].effects, {type = 'unlock-recipe', name = 'smart-inserter-arm'})
-table.insert(data.raw['tehcnology']['electronics'].effects, {type = 'unlock-recipe', name = 'smart-inserter-platform'})
-
-table.insert(data.raw['technology']['military'].effects, {type = 'unlock-recipe', name = 'blank-shotgun-shell'})
-table.insert(data.raw['technology']['military-2'].effects, {type = 'unlock-recipe', name = 'piercing-bullet'})
-table.insert(data.raw['technology']['military-3'].effects, {type = 'unlock-recipe', name = 'blank-capsule'})
-
-table.insert(data.raw['technology']['oil-processing'].effects, {type = 'unlock-recipe', name = 'compressed-solid-fuel'})
-
-table.insert(data.raw['technology']['speed-module'].effects, {type = 'unlock-recipe', name = 'speed-augment'})
-table.insert(data.raw['technology']['speed-module'].effects, {type = 'unlock-recipe', name = 'speed-module-casing'})
-table.insert(data.raw['technology']['productivity-module'].effects, {type = 'unlock-recipe', name = 'productivity-augment'})
-table.insert(data.raw['technology']['productivity-module'].effects, {type = 'unlock-recipe', name = 'productivity-module-casing'})
-table.insert(data.raw['technology']['effectivity-module'].effects, {type = 'unlock-recipe', name = 'effectivity-augment'})
-table.insert(data.raw['technology']['effectivity-module'].effects, {type = 'unlock-recipe', name = 'effectivity-module-casing'})
-=======
->>>>>>> origin/master
