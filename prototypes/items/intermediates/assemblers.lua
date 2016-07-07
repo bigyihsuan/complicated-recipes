8ac1e8ae5c20616155bd88c5e3e2a4cb9bbc72c4
bigyihsuan <bigyihsuan@gmail.com>
2016-07-07 20:15:44 +0800

    i forgot to check assembers

--- prototypes/items/intermediates/assemblers.lua
+++ prototypes/items/intermediates/assemblers.lua
@@ -4,7 +4,7 @@ data:extend({
 	flags= { "goes-to-main-inventory" },
 	name = 'assembling-machine-1-frame',
 	icon = '__complicated-recipes__/graphics/assemblers/assembling-machine-1-frame.png',
-	subgroup = 'CR-intermediates',
+	subgroup = 'assemblers',
 	inventory_order = 'a[assemblers]-a[assembling-machine-1]',
 	stack_size = 50
 },
@@ -13,7 +13,7 @@ data:extend({
 	flags= { "goes-to-main-inventory" },
 	name = 'assembling-machine-2-frame',
 	icon = '__complicated-recipes__/graphics/assemblers/assembling-machine-2-frame.png',
-	subgroup = 'CR-intermediates',
+	subgroup = 'assemblers',
 	inventory_order = 'a[assemblers]-b[assembling-machine-2]',
 	stack_size = 50
 },
@@ -22,7 +22,7 @@ data:extend({
 	flags= { "goes-to-main-inventory" },
 	name = 'assembling-machine-3-frame',
 	icon = '__complicated-recipes__/graphics/assemblers/assembling-machine-3-frame.png',
-	subgroup = 'CR-intermediates',
+	subgroup = 'assemblers',
 	inventory_order = 'a[assemblers]-c[assembling-machine-3]',
 	stack_size = 50
 } --[[,
