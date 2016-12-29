52b3b758975414d9e561cfa4fc14a2b9b2860d32
bigyihsuan <bigyihsuan@gmail.com>
2016-12-29 11:25:53 -0500

    Electrical Changes done
    Changed electric pole recipes, solar panel recipes, accumulator recipes

---
+++ prototypes/items/intermediates/electric.lua
@@ -0,0 +1,34 @@
+data:extend({
+	{
+		type = 'item',
+		name = 'steel-rebar',
+		flags = {'goes-to-main-inventory'},
+		icon = '__complicated-recipes__/graphics/electrics/steel-rebar.png', --icon directory
+		subgroup = 'intermediates',
+		stack_size = 50
+	},
+	{
+		type = 'item',
+		name = 'copper-contact',
+		flags = {'goes-to-main-inventory'},
+		icon = '__complicated-recipes__/graphics/electrics/copper-contact.png', --icon directory
+		subgroup = 'intermediates',
+		stack_size = 100
+	},
+	{
+		type = 'item',
+		name = 'photovoltaic-cell',
+		flags = {'goes-to-main-inventory'},
+		icon = '__complicated-recipes__/graphics/electrics/photovoltaic-cell.png', --icon directory
+		subgroup = 'intermediates',
+		stack_size = 100
+	},
+	{
+		type = 'item',
+		name = 'high-voltage-battery',
+		flags = {'goes-to-main-inventory'},
+		icon = '__complicated-recipes__/graphics/electrics/high-voltage-battery.png', --icon directory
+		subgroup = 'intermediates',
+		stack_size = 50
+	}
+})
---
+++ prototypes/recipes/electric.lua
@@ -0,0 +1,74 @@
+data:extend({
+	{
+		type = 'recipe',
+		name = 'steel-rebar',
+		ingredients = {
+			{'iron-stick', 10},
+			{'steel-plate', 2}
+		},
+		result = 'steel-rebar',
+		result_count = 2,
+		enabled = false --electric-energy-distribution-1
+	},
+	{
+		type = 'recipe',
+		name = 'copper-contact',
+		ingredients = {
+			{'copper-plate', 5},
+			{'copper-wire', 10}
+		},
+		result = 'copper-contact',
+		result_count = 1,
+		enabled = false --electric-energy-distribution-1
+	},
+	{
+		type = 'recipe',
+		name = 'high-voltage-battery',
+		ingredients = {
+			{'battery', 10},
+			{'steel-plate', 5},
+			{'copper-wire', 10}
+		},
+		result = 'high-voltage-battery',
+		result_count = 1,
+		enabled = false --electric-energy-accumulators
+	},
+	{
+		type = 'recipe',
+		name = 'photovoltaic-cell',
+		ingredients = {
+			{'iron-plate', 1},
+			{'blue-dye', 1},
+			{'copper-wire', 1}
+		},
+		result = 'photovoltaic-cell',
+		result_count = 1,
+		enabled = false --solar-energy
+	}
+})
+
+--changes
+data.raw.recipe['solar-panel'].ingredients = {
+	{'photovoltaic-cell', 9},
+	{'iron-plate', 5},
+	{'electronic-circuit', 5}
+}
+data.raw.recipe['accumulator'].ingredients = {
+	{'high-voltage-battery', 4},
+	{'steel-plate', 5},
+	{'copper-wire', 10}
+}
+data.raw.recipe['medium-electric-pole'].ingredients = {
+	{'steel-rebar', 10},
+	{'copper-contact', 3}
+}
+data.raw.recipe['large-electric-pole'].ingredients = {
+	{'steel-rebar', 20},
+	{'copper-contact', 6}
+}
+data.raw.recipe['substation'].ingredients = {
+	{'steel-rebar', 40},
+	{'copper-contact', 12},
+	{'advanced-circuit', 2},
+	{'steel-plate', 5}
+}
